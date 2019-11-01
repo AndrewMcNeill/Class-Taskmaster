867704421cf2654b065458bba756bad2c3fa8abe
Andrew McNeill <andrewjndmcneill@gmail.com>
2019-11-01 09:52:59 -0400

    Removed auto-generated files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,16 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = new BorderPane();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
